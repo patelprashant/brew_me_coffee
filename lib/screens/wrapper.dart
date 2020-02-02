5226ef8949e39825a26bf7bb9463fbd7eb59d214
Prashant <prashant.patel77@gmail.com>
2020-02-02 12:25:49 +1100

    Added Wrapper and screens

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:brew_me_coffee/screens/auth/authentication.dart';
+import 'package:brew_me_coffee/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //TODO: return home or authenticate screen
+    return Authenticate();
+  }
+}
