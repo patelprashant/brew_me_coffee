7c73db5f692c16eb7da1bd0a9eaa46f53562b044
Prashant <prashant.patel77@gmail.com>
2020-02-04 21:41:34 +1100

    Added Sign in form with fields.

--- lib/screens/authentication/sign_in.dart
+++ lib/screens/authentication/sign_in.dart
@@ -9,6 +9,10 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _authService = AuthService();
 
+  //text field state
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,19 +24,59 @@ class _SignInState extends State<SignIn> {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 48.0),
-        child: RaisedButton(
-          child: Text('Sign in Anon'),
-          onPressed: () async {
-            dynamic result = await _authService.signInAnon();
-            if (result == null) {
-              print('Error signing in anon');
-            } else {
-              print('Signing in anon');
-              print(result.uid);
-            }
-          },
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 24.0),
+              TextFormField(
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+                obscureText: true,
+              ),
+              SizedBox(height: 16.0),
+              RaisedButton(
+                onPressed: () async {
+                  print(email);
+                  print(password);
+
+//                  dynamic result = await _authService.signInAnon();
+//                  if (result == null) {
+//                    print('Error signing in anon');
+//                  } else {
+//                    print('Signing in anon');
+//                    print(result.uid);
+//                  }
+                },
+                color: Colors.orangeAccent,
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+//RaisedButton(
+//child: Text('Sign in Anon'),
+//onPressed: () async {
+//dynamic result = await _authService.signInAnon();
+//if (result == null) {
+//print('Error signing in anon');
+//} else {
+//print('Signing in anon');
+//print(result.uid);
+//}
+//},
+//)
